ac31ee3d2f2dce17a2b8589b8c3fcfb00385de6f
Florian Kreuk <fkreuk@cartermario.com>
2023-06-19 19:16:56 -0400

    Added the numerical scroll bar with 8-128 as selections - changed styling - gave credit to source in readme

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # password-generator
 
+For the css reset, I utilized the sample provided in 28-Stu_Mini-Project through the UCONN coding bootcamp.
 
 For the Hide/Show button utilized to display or hide the password criteria, I utilized the below resource/tutorial as a starting place: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
 
 For the checkboxes within the password criteria, I utilized the following resource/tutorial as a starting place: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/checkbox
+
+For the numerical slider, I utilized the following resource/tutorial as a starting place: https://www.w3schools.com/howto/howto_js_rangeslider.asp
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 // Assignment code here
 
+// Below is the code that will hide or display the password criteria contained within
+// myDIV when the Generate Password button is pressed.
+
 function myFunction() { 
   var x = document.getElementById("myDIV"); 
   if (x.style.display === "none") { 
@@ -9,6 +12,14 @@ function myFunction() {
   } 
 }
 
+// Below is the code that will update the slider value when the slider handle is dragged
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("length"); 
+output.innerHTML = slider.value; 
+slider.oninput = function() {
+   output.innerHTML = this.value; 
+  }
 
 
 // Get references to the #generate element
