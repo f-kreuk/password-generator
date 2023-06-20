1b04b158568c63645866a2070838719edd7cdf89
Florian Kreuk <fkreuk@cartermario.com>
2023-06-20 13:11:14 -0400

    Updated function to produce random number. Having trouble make length = to variable initialvalue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 // Assignment code here
 
-let includelowercase = "abcdefghijklmnopqrstuvwxyz";
 let includeuppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let includelowercase = "abcdefghijklmnopqrstuvwxyz";
 let includenumbers = "1234567890";
 let includesymbols = "~!@#$%^&*()_-=+<>?";
 
-let booleonlowercase = false;
 let booleonuppercase = false;
+let booleonlowercase = false;
 let booleonnumbers = false;
 let booleonsymbols = false;
-let initialvalue = 0;
+let initialvalue = 8;
 
 // Below is the code that will hide or display the password criteria contained within
 // myDIV when the Generate Password button is pressed.
@@ -36,91 +36,114 @@ slider.oninput = function() {
 // Below is the code that checks if checkbox has been changed (https://html.form.guide/html-form/html-checkbox-events/)
 
 // Below changes the booleonuppercase value depending on if checkbox is checked or not.
+// It also calls the createpool function
 function checkUppercase()
 {
   var checkbox = document.getElementById('uppercase');
   if (checkbox.checked != true)
   {
     booleonuppercase = false;
-    console.log("uppercase " + booleonuppercase); 
+    console.log("uppercase " + booleonuppercase);
+    includeuppercase = "";
+    createpool(); 
   } else {
     booleonuppercase = true;
     console.log("uppercase " + booleonuppercase);
+    includeuppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    createpool();
     }
   }
 
 // Below changes the booleonlowercase value depending on if checkbox is checked or not.
+// It also calls the createpool function
   function checkLowercase()
   {
     var checkbox = document.getElementById('lowercase');
     if (checkbox.checked != true)
     {
       booleonlowercase = false;
-      console.log("lowercase " + booleonlowercase); 
+      console.log("lowercase " + booleonlowercase);
+      includelowercase = "";
+      createpool(); 
     } else {
       booleonlowercase = true;
       console.log("lowercase " + booleonlowercase);
+      includelowercase = "abcdefghijklmnopqrstuvwxyz";
+      createpool();
       }
     }
 
 // Below changes the booleonnumbers value depending on if checkbox is checked or not.
+// It also calls the createpool function
     function checkNumbers()
     {
       var checkbox = document.getElementById('numbers');
       if (checkbox.checked != true)
       {
         booleonnumbers = false;
-        console.log("numbers " + booleonnumbers); 
+        console.log("numbers " + booleonnumbers);
+        includenumbers = "";
+        createpool(); 
       } else {
         booleonnumbers = true;
         console.log("numbers " + booleonnumbers);
+        includenumbers = "1234567890";
+        createpool();
         }
       }
 
 // Below changes the booleonsymbols value depending on if checkbox is checked or not.
+// It also calls the createpool function
       function checkSymbols()
       {
         var checkbox = document.getElementById('symbols');
         if (checkbox.checked != true)
         {
           booleonsymbols = false;
-          console.log("symbols " + booleonsymbols); 
+          console.log("symbols " + booleonsymbols);
+          includesymbols = "";
+          createpool(); 
         } else {
           booleonsymbols = true;
           console.log("symbols " + booleonsymbols);
+          includesymbols = "~!@#$%^&*()_-=+<>?";
+          createpool();
           }
         }
 
 // Below is the code that gathers the value for password length.
+// It also calls the createpool function
 
 function checkValue() 
 {
   var initialvalue = document.getElementById('myRange').valueAsNumber;
   console.log(initialvalue);
+  initialvalue = 
+  createpool();
 }
 
 // Below is the code that decides which characters are included in our pool for the password generation.
 
 let passwordpool = "";
 
+function createpool()
+
+{ 
+  console.log(includeuppercase + includelowercase + includenumbers + includesymbols);
+  console.log(initialvalue);
+
+  // Below is the code that generates the random string
 
 
-// Below is the code that generates the random string
-
-const generateRandomString = (length) => { let result = '';
-const characters =
-'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-const charactersLength = characters.length;
-for (let i = 0; i < length; i++) { result += characters.charAt(Math.floor(Math.random() * charactersLength));
-}
-return result;
-};
-
-console.log(generateRandomString(5));
-console.log(generateRandomString(30));
-
-
-
+  const generateRandomString = (length) => { let result = '';
+  const characters = (includeuppercase + includelowercase + includenumbers + includesymbols);
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) { result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+  };
+  console.log(8);
+ }
 
 
 
