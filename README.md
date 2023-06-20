733bfe5ad4baf8a7df63904cce9d3dc2fe6b0f73
Florian Kreuk <fkreuk@cartermario.com>
2023-06-19 21:27:50 -0400

    Added functions to change a variable to true/false depending on if checkboxes were checked or not

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # password-generator
 
+I modeled mine after the LastPass password generator feature. https://lastpass.com/?ac=1&lpnorefresh=1. Picture of an example is seen here: link.
+
 For the css reset, I utilized the sample provided in 28-Stu_Mini-Project through the UCONN coding bootcamp.
 
 For the Hide/Show button utilized to display or hide the password criteria, I utilized the below resource/tutorial as a starting place: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
@@ -7,4 +9,8 @@ For the Hide/Show button utilized to display or hide the password criteria, I ut
 For the checkboxes within the password criteria, I utilized the following resource/tutorial as a starting place: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/checkbox
 
 For the numerical slider, I utilized the following resource/tutorial as a starting place: https://www.w3schools.com/howto/howto_js_rangeslider.asp
+
+For the function to change a booleon value when a checkbox was checked or not, I utilized the following resource/tutorial as a starting place: https://html.form.guide/html-form/html-checkbox-events/
+
+To generate the random string, I utilized the below tutorial/resource as a starting place: https://www.slingacademy.com/article/ways-to-generate-random-strings-in-javascript/#:~:text=We%20can%20use%20the%20Math,using%20the%20charAt()%20function
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 // Assignment code here
 
+let includelowercase = "abcdefghijklmnopqrstuvwxyz";
+let includeuppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let includenumbers = "1234567890";
+let includesymbols = "~!@#$%^&*()_-=+<>?";
+
+let booleonlowercase = false;
+let booleonuppercase = false;
+let booleonnumbers = false;
+let booleonsymbols = false;
+
+console.log(includelowercase);
+console.log(includeuppercase);
+console.log(booleonlowercase);
+
 // Below is the code that will hide or display the password criteria contained within
 // myDIV when the Generate Password button is pressed.
 
@@ -12,7 +26,8 @@ function myFunction() {
   } 
 }
 
-// Below is the code that will update the slider value when the slider handle is dragged
+// Below is the code that will update the slider value when the slider handle is dragged.
+// This is for password length.
 
 var slider = document.getElementById("myRange");
 var output = document.getElementById("length"); 
@@ -20,6 +35,83 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
    output.innerHTML = this.value; 
   }
+
+// Below is the code that checks if checkbox has been changed (https://html.form.guide/html-form/html-checkbox-events/)
+
+// Below changes the booleonuppercase value depending on if checkbox is checked or not.
+function checkUppercase()
+{
+  var checkbox = document.getElementById('uppercase');
+  if (checkbox.checked != true)
+  {
+    booleonuppercase = false;
+    console.log("uppercase " + booleonuppercase); 
+  } else {
+    booleonuppercase = true;
+    console.log("uppercase " + booleonuppercase);
+    }
+  }
+
+// Below changes the booleonlowercase value depending on if checkbox is checked or not.
+  function checkLowercase()
+  {
+    var checkbox = document.getElementById('lowercase');
+    if (checkbox.checked != true)
+    {
+      booleonlowercase = false;
+      console.log("lowercase " + booleonlowercase); 
+    } else {
+      booleonlowercase = true;
+      console.log("lowercase " + booleonlowercase);
+      }
+    }
+
+// Below changes the booleonnumbers value depending on if checkbox is checked or not.
+    function checkNumbers()
+    {
+      var checkbox = document.getElementById('numbers');
+      if (checkbox.checked != true)
+      {
+        booleonnumbers = false;
+        console.log("numbers " + booleonnumbers); 
+      } else {
+        booleonnumbers = true;
+        console.log("numbers " + booleonnumbers);
+        }
+      }
+
+// Below changes the booleonsymbols value depending on if checkbox is checked or not.
+      function checkSymbols()
+      {
+        var checkbox = document.getElementById('symbols');
+        if (checkbox.checked != true)
+        {
+          booleonsymbols = false;
+          console.log("symbols " + booleonsymbols); 
+        } else {
+          booleonsymbols = true;
+          console.log("symbols " + booleonsymbols);
+          }
+        }
+
+// Below is the code that generates the random string
+
+const generateRandomString = (length) => { let result = '';
+const characters =
+'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const charactersLength = characters.length;
+for (let i = 0; i < length; i++) { result += characters.charAt(Math.floor(Math.random() * charactersLength));
+}
+return result;
+};
+
+console.log(generateRandomString(5));
+console.log(generateRandomString(30));
+
+
+
+
+
 
 
 // Get references to the #generate element
